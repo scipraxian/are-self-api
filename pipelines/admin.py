756627b9bff1a2e6bc95ba6138a7a96df4309313
GeneralDread <michael@hauntedspacehotel.com>
2026-01-06 15:33:05 -0800

    Add admin for step run so i can see it.

--- pipelines/admin.py
+++ pipelines/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import BuildProfile, PipelineRun
+from .models import BuildProfile, PipelineRun, PipelineStepRun
 
 @admin.register(BuildProfile)
 class BuildProfileAdmin(admin.ModelAdmin):
@@ -10,3 +10,7 @@ class PipelineRunAdmin(admin.ModelAdmin):
     list_display = ('id', 'profile', 'status', 'created_at')
     list_filter = ('status', 'profile')
     readonly_fields = ('created_at', 'celery_task_id')
+
+@admin.register(PipelineStepRun)
+class PipelineStepRunAdmin(admin.ModelAdmin):
+    list_display = ('step_name', 'status')
