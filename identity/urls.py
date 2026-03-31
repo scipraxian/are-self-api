32f2c64356c02ed32a19cc9cc45ac623ca8cf550
GeneralDread <michael@hauntedspacehotel.com>
2026-03-31 15:59:21 -0700

    Missing urls.

--- identity/urls.py
+++ identity/urls.py
@@ -1,6 +1,12 @@
 from rest_framework import routers
 
-from identity.api import IdentityDiscViewSet, IdentityViewSet
+from identity.api import (
+    IdentityAddonViewSet,
+    IdentityDiscViewSet,
+    IdentityTagViewSet,
+    IdentityTypeViewSet,
+    IdentityViewSet,
+)
 
 V2_IDENTITY_ROUTER = routers.SimpleRouter()
 V2_IDENTITY_ROUTER.register(
@@ -8,4 +14,13 @@ V2_IDENTITY_ROUTER.register(
 )
 V2_IDENTITY_ROUTER.register(
     r'identity-discs', IdentityDiscViewSet, basename='identity-discs'
+)
+V2_IDENTITY_ROUTER.register(
+    r'identity-addons', IdentityAddonViewSet, basename='identity-addons'
+)
+V2_IDENTITY_ROUTER.register(
+    r'identity-tags', IdentityTagViewSet, basename='identity-tags'
+)
+V2_IDENTITY_ROUTER.register(
+    r'identity-types', IdentityTypeViewSet, basename='identity-types'
 )
